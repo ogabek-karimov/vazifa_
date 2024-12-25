aa5d45143ad186f4ec9a63334140c0a2575e4ed2
Og'abek <asd@.com>
2024-12-25 09:12:28 +0500

    birinchi commit

--- main.js
+++ main.js
@@ -1,15 +1,20 @@
 let items = document.querySelectorAll(".item");
 let box = document.querySelector(".box");
-let btn = document.querySelector(".btn");
 
-btn.addEventListener("click", function () {
-  items.forEach((item, index) => {
-    item.addEventListener("click", function () {
-      let imgSrc = this.querySelector("img").src;
-      box.style.backgroundImage = `url(${imgSrc})`;
-      box.style.backgroundSize = "contain";
-      box.style.backgroundRepeat = "no-repeat";
-      box.style.backgroundPosition = "center";
-    });
+// Har bir item uchun event listener o‘rnatamiz
+items.forEach((item) => {
+  item.addEventListener("click", function () {
+    let imgSrc = this.querySelector("img").src;
+    box.style.backgroundImage = `url(${imgSrc})`;
+    box.style.backgroundSize = "contain";
+    box.style.backgroundRepeat = "no-repeat";
+    box.style.backgroundPosition = "center";
   });
+});
+
+// Btn uchun alohida event listener o‘rnatamiz
+let btn = document.querySelector(".btn");
+btn.addEventListener("click", function () {
+  // Harakatni bu yerda bajarishingiz mumkin, agar kerak bo‘lsa
+  console.log("Button clicked");
 });
